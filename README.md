d368f859bf9addcac684e89178c457c04e6dd179
Ji Hyuk Ma <t90laser93@gmail.com>
2022-02-17 17:11:19 +0900

    : Header/Home/Section components created

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-#### 유투브 리액트 강의
+# 유투브 리액트 강의
+
+## 배운내용
+
+#### CSS
+
+- justify-content: space-between 으로 동일한 간격으로 펼쳐져 있는 요소 3개 중 2개의 거리를 더 좁히고 싶을때 -> 두 개의 요소를 하나의
+  container로 묶어주면 됨(해당 컨테이너가 나머지 한 개의 요소와 동일한 간격을 가지게 됨)
+
+- creating animations using keyframes
---
+++ src/components/Section.js
@@ -0,0 +1,88 @@
+import React from "react";
+import styled from "styled-components";
+
+function Section({
+  title,
+  description,
+  leftBtnText,
+  rightBtnText,
+  backgroundImg,
+}) {
+  return (
+    <Wrap bgImage={backgroundImg}>
+      <ItemText>
+        <h1>{title}</h1>
+        <p>{description}</p>
+      </ItemText>
+      <Buttons>
+        <ButtonGroup>
+          <LeftButton>{leftBtnText}</LeftButton>
+          <RightButton>{rightBtnText}</RightButton>
+        </ButtonGroup>
+        <DownArrow src="/images/down-arrow.svg" />
+      </Buttons>
+    </Wrap>
+  );
+}
+
+export default Section;
+
+const Wrap = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-image: url("./images/model-s.jpg");
+  display: flex;
+
+  // flex는 원래 왼쪽에서 오른쪽으로 요소들이 차례로 정렬되는데, column 으로 설정해주게 되면 위에서 아래로 설정됨
+  //
+  flex-direction: column;
+  justify-content: space-between; // 원래는 horizontal 배치에 관여하는 속성이지만, 지금은 방향이 바뀌어있으므로 vertical 속성에 해당됨
+  align-items: center; // horizontal alignment
+  background-image: ${(props) => `url("/images/${props.bgImage}")`};
+`;
+
+const ItemText = styled.div`
+  padding-top: 15vh;
+  text-align: center;
+`;
+
+const ButtonGroup = styled.div`
+  display: flex;
+  margin-bottom: 30px;
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
+
+const LeftButton = styled.div`
+  background-color: rgba(23, 26, 32, 0.8);
+  height: 40px;
+  width: 256px;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 100px;
+  opacity: 0.85;
+  text-transform: uppercase;
+  font-size: 12px;
+  cursor: pointer;
+  margin: 8px;
+`;
+
+const RightButton = styled(LeftButton)`
+  background: white;
+  opacity: 0.65;
+  color: black;
+`;
+
+const DownArrow = styled.img`
+  height: 40px;
+  animation: animateDown infinite 1.5s;
+  overflow-x: hidden;
+`;
+
+const Buttons = styled.div``;
